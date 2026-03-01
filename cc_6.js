3aa211d6e779a7f6fb4bf4d0173dceebd143e57a
Shirley <your.email@usf.edu>
2026-03-01 18:06:55 -0500

    fixed inheritance errors and implemented Company listEmployees method

--- cc_6.js
+++ cc_6.js
@@ -1,27 +1,29 @@
-class Employee {     //base for employee class
-    constructor(name, departemnt) {
+class Employee {     // base for employee class
+    constructor(name, department) {
         this.name = name;
         this.department = department;
     }
+
     describe() {
         return `${this.name} works in the ${this.department} department.`;
     }
 }
 
-class Manager extends Employee {  //subclass under employee for managers
-    constructor(name, departement, teamSize) {
-        this.name = name;
-        this.department = department;
+class Manager extends Employee {  // subclass under employee for managers
+    constructor(name, department, teamSize) {
+        super(name, department);   
+        this.teamSize = teamSize;  // manager team size
     }
-    describe() {
-        return `${this.name} works in the ${this.department} department.`;
+
+    describe() {  
+        return `${this.name} manages the ${this.department} department and leads a team of ${this.teamSize} employees.`;
     }
 }
 
-const emp1 = new Employee("Tina Fox", "Marketing");
+const emp1 = new Employee("Tina Fox", "Marketing"); //employee samples
 const emp2 = new Employee("John Wolf", "Finance");
 
-const mgr1 = new Manager("David Toads", "IT", 7);
+const mgr1 = new Manager("David Toads", "IT", 7);  //manager samples
 const mgr2 = new Manager("Allison Bee", "HR", 5);
 
 class Company {
@@ -29,12 +31,22 @@ class Company {
         this.employees = [];
     }
 
-addEmployee(employee) {
-    this.employees.push(employee);
+    addEmployee(employee) {
+        this.employees.push(employee);
     }
-listEmployees() {
-    this.employees.forEach(em => {
-            console.log(emp.describe());
+
+    listEmployees() {
+        this.employees.forEach(emp => {
+            console.log(emp.describe());  // fixed variable name
         });
     }
 }
+
+const myCompany = new Company();
+
+myCompany.addEmployee(emp1);
+myCompany.addEmployee(emp2);
+myCompany.addEmployee(mgr1);
+myCompany.addEmployee(mgr2);
+
+myCompany.listEmployees();
