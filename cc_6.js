385b8868b87e57e1bcf50c434960bfdd8c283684
Shirley <your.email@usf.edu>
2026-03-01 17:58:17 -0500

    added sample Employee and Manager instances and created Company class

--- cc_6.js
+++ cc_6.js
@@ -8,12 +8,33 @@ class Employee {     //base for employee class
     }
 }
 
-class Manager extends Employees {  //subclass under employee for managers
+class Manager extends Employee {  //subclass under employee for managers
     constructor(name, departement, teamSize) {
         this.name = name;
         this.department = department;
     }
     describe() {
         return `${this.name} works in the ${this.department} department.`;
+    }
+}
+
+const emp1 = new Employee("Tina Fox", "Marketing");
+const emp2 = new Employee("John Wolf", "Finance");
+
+const mgr1 = new Manager("David Toads", "IT", 7);
+const mgr2 = new Manager("Allison Bee", "HR", 5);
+
+class Company {
+    constructor() {
+        this.employees = [];
+    }
+
+addEmployee(employee) {
+    this.employees.push(employee);
+    }
+listEmployees() {
+    this.employees.forEach(em => {
+            console.log(emp.describe());
+        });
     }
 }
